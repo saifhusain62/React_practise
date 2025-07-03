8837f8d9ebda5615fedff6cfa90af64b61f522d8
saifhusain62 <saimumsaif62@gmail.com>
2025-07-03 10:07:38 +0600

    Ifelse component

---
+++ project/src/components/Ifelse.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+   let marks = 60;
+ 
+function Ifelse() {
+  return (
+    <div>
+        {marks>80?<h1>Brilliant</h1>:<h1>Need Improved</h1>}
+        
+    </div>
+  )
+}
+
+export default Ifelse
